6afc9cb89aa6126f0e97f1f2679915e31c0cc68f
Dieter Blancke <dieter.blancke@outlook.com>
2019-08-01 08:46:21 +0200

    new design

---
+++ resources/views/home.blade.php
@@ -0,0 +1,145 @@
+@extends('template.main')
+
+@section('header')
+    <div id="nav-wrapper">
+        <h1>BungeeUtilisalsX</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Users</a></li>
+                <li><a href="#">Bans</a></li>
+                <li><a href="#">Mutes</a></li>
+                <li><a href="#">Kicks</a></li>
+                <li><a href="#">Warns</a></li>
+                <li><a href="#">Punishment Actions</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <div id="welcome">
+        <h2>Welcome to the BungeeUtilisals Web UI</h2>
+        <p>
+            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, dicta doloremque earum enim similique tenetur.
+                Atque autem culpa, ea excepturi nostrum repellat? Eius et eveniet non porro suscipit tempore. Deserunt.</span>
+            <span>Adipisci aut debitis, delectus, earum eos excepturi itaque molestias nam praesentium, quidem quod sapiente unde voluptatem?
+                Accusantium adipisci corporis et, eum ex, illum laudantium minus molestias omnis quam reiciendis voluptatum.</span>
+            <span>Dolore facilis minus nemo neque tempora! Ad beatae eum explicabo fugit in incidunt mollitia necessitatibus quam quia quo quod,
+                rerum sunt suscipit tempore unde.Cumque magni optio perferendis quaerat quas.</span>
+        </p>
+    </div>
+
+    <div class="wrapper"></div>
+@endsection()
+
+@section('content')
+    <div id="recentpunishments">
+        <ul>
+            <li>
+                <article>
+                    <h3>Latest Bans</h3>
+
+                    <ul>
+                        <li>
+                            <a href="//stats.staging.centrixpvp.eu/punishments/player/XxSoulCreeperxX">
+                                <img src="//cravatar.eu/head/didjee2/32" alt="Player Skull" title="Player Skull">
+                                <span class="name">didjee2</span>
+                                <span class="date temporary-green">2019-07-31 21:26:15</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="//stats.staging.centrixpvp.eu/punishments/player/XxSoulCreeperxX">
+                                <img src="//cravatar.eu/head/didjee2/32" alt="Player Skull" title="Player Skull">
+                                <span class="name">didjee2</span>
+                                <span class="date permanent-red">Permanent</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="//stats.staging.centrixpvp.eu/punishments/player/XxSoulCreeperxX">
+                                <img src="//cravatar.eu/head/didjee2/32" alt="Player Skull" title="Player Skull">
+                                <span class="name">didjee2</span>
+                                <span class="date temporary-green">2019-07-31 21:26:15</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="//stats.staging.centrixpvp.eu/punishments/player/XxSoulCreeperxX">
+                                <img src="//cravatar.eu/head/didjee2/32" alt="Player Skull" title="Player Skull">
+                                <span class="name">didjee2</span>
+                                <span class="date temporary-green">2019-07-31 21:26:15</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="//stats.staging.centrixpvp.eu/punishments/player/XxSoulCreeperxX">
+                                <img src="//cravatar.eu/head/didjee2/32" alt="Player Skull" title="Player Skull">
+                                <span class="name">didjee2</span>
+                                <span class="date permanent-red">Permanent</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="//stats.staging.centrixpvp.eu/punishments/player/XxSoulCreeperxX">
+                                <img src="//cravatar.eu/head/didjee2/32" alt="Player Skull" title="Player Skull">
+                                <span class="name">didjee2</span>
+                                <span class="date temporary-green">2019-07-31 21:26:15</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="//stats.staging.centrixpvp.eu/punishments/player/XxSoulCreeperxX">
+                                <img src="//cravatar.eu/head/didjee2/32" alt="Player Skull" title="Player Skull">
+                                <span class="name">didjee2</span>
+                                <span class="date permanent-red">Permanent</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="//stats.staging.centrixpvp.eu/punishments/player/XxSoulCreeperxX">
+                                <img src="//cravatar.eu/head/didjee2/32" alt="Player Skull" title="Player Skull">
+                                <span class="name">didjee2</span>
+                                <span class="date permanent-red">Permanent</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="//stats.staging.centrixpvp.eu/punishments/player/XxSoulCreeperxX">
+                                <img src="//cravatar.eu/head/didjee2/32" alt="Player Skull" title="Player Skull">
+                                <span class="name">didjee2</span>
+                                <span class="date permanent-red">Permanent</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="//stats.staging.centrixpvp.eu/punishments/player/XxSoulCreeperxX">
+                                <img src="//cravatar.eu/head/didjee2/32" alt="Player Skull" title="Player Skull">
+                                <span class="name">didjee2</span>
+                                <span class="date temporary-green">2019-07-31 21:26:15</span>
+                            </a>
+                        </li>
+                    </ul>
+                </article>
+            </li>
+            <li>
+                <article>
+                    <h3>Latest Mutes</h3>
+
+                    <ul>
+                        <li>didjee2</li>
+                        <li>didjee2</li>
+                        <li>didjee2</li>
+                        <li>didjee2</li>
+                        <li>didjee2</li>
+                        <li>didjee2</li>
+                        <li>didjee2</li>
+                        <li>didjee2</li>
+                        <li>didjee2</li>
+                        <li>didjee2</li>
+                    </ul>
+                </article>
+            </li>
+            <li>
+                <article>
+                    <h3>Latest Kicks & Warns</h3>
+
+                    <ul>
+                        <li>didjee2</li>
+                        <li>didjee2</li>
+                        <li>didjee2</li>
+                    </ul>
+                </article>
+            </li>
+        </ul>
+    </div>
+@endsection
---
+++ resources/views/template/main.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="author" content="Dieter Blancke"/>
+    <link rel="icon" type="image/png" href="{{ asset("images/bulogo.png") }}"/>
+
+    <title>@yield('title')</title>
+
+    <link rel="stylesheet" href="{{ asset("css/reset.css") }}"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="{{ asset("css/app.css") }}"/>
+    @yield('css')
+</head>
+<body>
+
+<header>
+    @yield('header')
+</header>
+<main>
+    @yield('content')
+</main>
+<footer class="page-footer">
+    <div class="container" style="padding-bottom: 0">
+        <p>
+            © 2018 - {{ date("Y") }} BungeeUtilisalsX -
+            Site made with <span style="color: #c60000;">&hearts;</span> by
+            <a href="//www.dbsoftwares.eu">DBSoftwares</a>.
+        </p>
+    </div>
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous">
+</script>
+<script src="{{ asset('js/particle.min.js') }}"></script>
+<script src="{{ asset('js/main.js') }}"></script>
+
+@yield('scripts')
+</body>
+</html>
